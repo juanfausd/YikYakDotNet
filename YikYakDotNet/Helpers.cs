d8339bb16905e9c6cce3a907279981b2378da835
Juan Alejandro Fausd <juan.fausd@gmail.com>
2014-10-09 14:28:54 -0300

    Changes on generating user id

--- YikYakDotNet/Helpers.cs
+++ YikYakDotNet/Helpers.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -30,6 +31,40 @@ namespace YikYakDotNet
             }
 
             return responseData;
+        }
+
+        public static double ConvertToUnixTimestamp(DateTime date)
+        {
+            DateTime origin = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+            TimeSpan diff = date.ToUniversalTime() - origin;
+            return Math.Ceiling(diff.TotalSeconds);
+        }
+
+        public static string Encode(string input, string key)
+        {
+            var keyMaterial = System.Text.Encoding.UTF8.GetBytes(key);
+            HMACSHA1 myhmacsha1 = new HMACSHA1(keyMaterial);
+            byte[] byteArray = System.Text.Encoding.UTF8.GetBytes(input);
+            MemoryStream stream = new MemoryStream(byteArray);
+            byte[] res = myhmacsha1.ComputeHash(stream);
+            var b64 = Convert.ToBase64String(res);
+            return System.Uri.EscapeDataString(b64);
+        }
+
+        public static string CalculateMD5Hash(string input)
+        {
+            // step 1, calculate MD5 hash from input
+            MD5 md5 = System.Security.Cryptography.MD5.Create();
+            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+            byte[] hash = md5.ComputeHash(inputBytes);
+
+            // step 2, convert byte array to hex string
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("X2"));
+            }
+            return sb.ToString();
         }
     }
 }
--- YikYakDotNet/YikYakAPI.cs
+++ YikYakDotNet/YikYakAPI.cs
@@ -13,29 +13,51 @@ namespace YikYakDotNet
     {
         private const string GET_MESSAGES_URL = "/api/getMessages?lat={latitude}&long={longitude}&userID={user-id}";
         private const string REGISTER_USER_URL = "/api/registerUser?lat={latitude}&long={longitude}&userID={user-id}";
-        private const string USER_AGENT = "android-async-http/1.4.4 (http://loopj.com/android-async-http)";
-
-        private string GenerateUserId()
-        {
-            return new Guid().ToString();
-        }
+        private const string USER_AGENT = "Mozilla/5.1 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.133 Mobile Safari/535.19";
+        private const string DEVICE_KEY = "35FD04E8-B7B1-45C4-9886-94A75F4A2BB4";
 
         public void Execute()
         {
-            string key = "35FD04E8-B7B1-45C4-9886-94A75F4A2BB4";
-            string salt = ConvertToUnixTimestamp(DateTime.Now).ToString();
+            string salt = Helpers.ConvertToUnixTimestamp(DateTime.Now).ToString();
+            string userId = Helpers.CalculateMD5Hash(Guid.NewGuid().ToString());
 
+            RegisterUser(userId, DEVICE_KEY, salt);
+
+            string result = GetMessages(userId, DEVICE_KEY, salt);
+        }
+
+        public void RegisterUser(string userId, string key, string salt)
+        {
+            string regUrl = REGISTER_USER_URL;
+            regUrl = regUrl.Replace("{latitude}", "38.832194");
+            regUrl = regUrl.Replace("{longitude}", "-77.308037");
+            regUrl = regUrl.Replace("{user-id}", userId);
+            string encodeRegUrl = regUrl;
+            encodeRegUrl += salt;
+
+            string regHash = Helpers.Encode(encodeRegUrl, key);
+            regUrl = regUrl + "&salt={salt}".Replace("{salt}", salt);
+            regUrl = regUrl + "&hash={hash}".Replace("{hash}", regHash);
+            regUrl = "https://yikyakapp.com" + regUrl;
+
+            HttpWebRequest regRequest = WebRequest.Create(regUrl) as HttpWebRequest;
+            regRequest.Method = "GET";
+            regRequest.UserAgent = USER_AGENT;
+            HttpWebResponse regResponse = (HttpWebResponse)regRequest.GetResponse();
+        }
+
+        public string GetMessages(string userId, string key, string salt)
+        {
             string url = GET_MESSAGES_URL;
-            url = url.Replace("{latitude}", "40.013842");
-            url = url.Replace("{longitude}", "-83.031085");
-            url = url.Replace("{user-id}", Guid.NewGuid().ToString().Replace("-", ""));
+            url = url.Replace("{latitude}", "38.832194");
+            url = url.Replace("{longitude}", "-77.308037");
+            url = url.Replace("{user-id}", userId);
             string encodeUrl = url;
             encodeUrl += salt;
 
-            string hash = Encode(encodeUrl, key);
+            string hash = Helpers.Encode(encodeUrl, key);
             url = url + "&salt={salt}".Replace("{salt}", salt);
             url = url + "&hash={hash}".Replace("{hash}", hash);
-
             url = "https://yikyakapp.com" + url;
 
             HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
@@ -43,25 +65,7 @@ namespace YikYakDotNet
             request.UserAgent = USER_AGENT;
             HttpWebResponse response = (HttpWebResponse)request.GetResponse();
 
-            string content = Helpers.ReadWebResponse(response);
-        }
-
-        public static double ConvertToUnixTimestamp(DateTime date)
-        {
-            DateTime origin = new DateTime(1970, 1, 1, 0, 0, 0, 0);
-            TimeSpan diff = date.ToUniversalTime() - origin;
-            return Math.Ceiling(diff.TotalSeconds);
-        }
-
-        public static string Encode(string input, string key)
-        {
-            var keyMaterial = System.Text.Encoding.UTF8.GetBytes(key);
-            HMACSHA1 myhmacsha1 = new HMACSHA1(keyMaterial);
-            byte[] byteArray = System.Text.Encoding.UTF8.GetBytes(input);
-            MemoryStream stream = new MemoryStream(byteArray);
-            byte[] res = myhmacsha1.ComputeHash(stream);
-            var b64 = Convert.ToBase64String(res);
-            return System.Uri.EscapeDataString(b64);
+            return Helpers.ReadWebResponse(response);
         }
     }
 }
