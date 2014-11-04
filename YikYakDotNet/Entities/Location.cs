7fc15d3afd2791f3f6a1e18421e9e1addcf592dd
Juan Alejandro Fausd <juan.fausd@gmail.com>
2014-11-04 10:02:09 -0300

    Added PeekMessages and PeekAnywhere support

---
+++ YikYakDotNet/Entities/Location.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YikYakDotNet.Entities
+{
+    public class Location
+    {
+        public string PeekID { get; set; }
+        public string Name { get; set; }
+        public bool Inactive { get; set; }
+        public bool CanSubmit { get; set; }
+        public bool CanVote { get; set; }
+        public bool CanReply { get; set; }
+        public bool CanReport { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public double Delta { get; set; }
+    }
+}
